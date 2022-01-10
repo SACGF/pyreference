74afadd195df08478801e4bf2c1111cccb3b7013
kpillman <kapillman@gmail.com>
2022-01-11 09:44:24 +1030

    ncludes changes in pyreference_gff_to_json.py to identify biotype in GRCh38 Ensembl annotation file, where it is stored under the "gene_type" attribute rather than "gene_biotype". Changes in pyreference_biotype.py to allow only genes which do not have a gene.name to bypass checking wheher these are yRNAs. Also includes changes to plotting and spreadsheet code to use seaborn and pandas.

--- bin/pyreference_biotype.py
+++ bin/pyreference_biotype.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
+"""
+Created on 22Jan.,2018
+
+@author: dlawrence
+"""
 
 from __future__ import print_function, absolute_import
 
@@ -15,11 +20,12 @@ from matplotlib.patches import Rectangle
 import os
 from pyreference import Reference
 from pyreference.utils import iv_iterators
-from pyreference.utils.file_utils import name_from_file_name, file_or_file_name
+from pyreference.utils.file_utils import name_from_file_name, file_or_file_name, \
+    mk_path_for_file
 from pyreference.utils.genomics_utils import opposite_strand, format_chrom
 import six
-import sys
-
+import seaborn as sns
+import pandas as pd
 import numpy as np
 
 MAX_READ_LENGTH = 200
@@ -80,8 +86,9 @@ def create_biotype_regions_array(reference, interesting_biotypes=None):
         if gene.biotype in other_biotypes:
             return "other"
         elif gene.biotype == "misc_RNA":
-            if gene.name.startswith("RNY"):
-                return "yRNA"
+            if gene.name:
+                if gene.name.startswith("RNY"):
+                    return "yRNA"
         return gene.biotype
 
     regions = HTSeq.GenomicArray("auto", stranded=True, typecode='O')
@@ -125,10 +132,16 @@ def add_intervals_to_regions_array(regions_array, intervals, intervals_name, has
 
 def main():
     args = handle_args()
-
+    
+    #Set up  
+    sample_name = name_from_file_name(args.bam)
+    csv_file = "%s.read_counts.regions.csv" % sample_name
+    graph_image =  "%s.read_counts.regions.png" % sample_name
+    
     # Use this as a test platform to load reference
     reference = Reference()
-
+    print("Reference is", reference) #To confirm the annotation you're using is what you intended. 
+    
     regions_array = create_biotype_regions_array(reference)
 
     if args.intervals:
@@ -177,240 +190,48 @@ def main():
             counts_for_lengths[l - start] = length_count
             total_counts[k] += length_count
         arrays.append(counts_for_lengths)
+        
+    df = pd.DataFrame(data=arrays, columns=largs, index=labels)
+    df = df.T
+    df.to_csv(csv_file)
 
-    graph_types = {"read_counts": lambda x: x}
-
-    sample_name = name_from_file_name(args.bam)
-
-    for (y_label, op) in six.iteritems(graph_types):
-        graph_image = "%s.%s.regions.png" % (sample_name, y_label)
-        data = [op(i) for i in arrays]
-
-        ncol = max(len(labels), 5)
-        legend_kwargs = {'loc': 'center left',
-                         'prop': {'size': 8},
-                         'bbox_to_anchor': (1.01, 0.5)}
-        write_stacked_bar_graph(graph_image, largs, data, labels, colors, y_label=y_label, legend_side_ratio=0.15,
-                                legend_kwargs=legend_kwargs)
-        write_individual_bar_graphs(graph_image, largs, data, labels, colors, y_label=y_label)
-
-    # Write region stats CSV
-    csv_file = "%s.biotype_stats.csv" % sample_name
-    write_csv_dict(csv_file, labels, [total_counts])
-
-
-"""
-Created on 22Jan.,2018
-
-@author: dlawrence
-"""
-
-
-def axis_stacked_bar_graph(ax, largs, arrays, labels, colors):
-    bottom = np.zeros(len(largs), dtype='i')
-    lines = None
-    for array, label, color in zip(arrays, labels, colors):
-        lines = ax.bar(largs, array, label=label, color=color, bottom=bottom, linewidth=0)
-        bottom += array
-
-    return lines
-
-
-def write_stacked_bar_graph(graph_image, largs, arrays, labels, colors, **kwargs):
-    """largs = x-values
-    arrays = list of lists y-values
-    labels = list of labels, same length as arrays
-    colors = list of colors, same length as arrays
-
-    kwargs:
-        extra_func = method called with args=(ax, fig, lines)
-        legend_side_ratio : eg 0.1 - Shrink figure by this
-        legend_kwargs : Passed to ax.legend
-    """
-    logging.info("write_stacked_bar_graph: %s", graph_image)
-
-    title = kwargs.get("title")
-    extra_func = kwargs.get("extra_func")
-    subtitle = kwargs.get("subtitle")
-    x_label = kwargs.get("x_label")
-    y_label = kwargs.get("y_label")
-    legend_side_ratio = kwargs.get("legend_side_ratio")
-
-    # Old kwargs, which we passed to legend
-    legend_kwargs = {"loc": kwargs.get("loc"),
-                     "title": kwargs.get("legend_title"),
-                     "prop": kwargs.get("legend_props", {})}
-    # Overwrite with legend_kwargs
-    legend_kwargs.update(kwargs.get("legend_kwargs", {}))
-
-    fig = Figure(dpi=300)
+    ### Graph data ###
+    sns.set_theme(context='paper', style="ticks", font_scale=1.1)
+    legend_kwargs = {'loc' : 'center left',
+                     'prop' : {'size': 8.5},
+                     'bbox_to_anchor' : (1.01, 0.5)}
+    
+#     df = pd.read_csv(csv_file, index_col=0)
+    print(df)
+     
+    fig = Figure(dpi=300, figsize=(4.8, 3.1))
+    
     fig.patch.set_facecolor('white')
     ax = fig.add_subplot(111)
-    lines = axis_stacked_bar_graph(ax, largs, arrays, labels, colors)
-
-    if title:
-        fig.suptitle(title, fontsize=18)
-    if subtitle:
-        ax.set_title(subtitle, fontsize=12)
-    if extra_func:
-        extra_func(ax, fig, lines)
-    if x_label:
-        ax.set_xlabel(x_label)
-    if y_label:
-        ax.set_ylabel(y_label)
-
-    ax.set_xlim(xmin=largs[0], xmax=largs[-1] + 1)
-
-    if legend_side_ratio is not None:
-        print("legend_side_ratio: %f" % legend_side_ratio)
-        assert 0 < legend_side_ratio < 1, "legend_side_ratio: 0 < %f < 1" % legend_side_ratio
-
-        # Shrink current axis's height by legend_below_ratio on the bottom
-        # ax = pyplot.axes()
-        box = ax.get_position()
-        print("Box w/h = (%f,%f)" % (box.width, box.height))
-
-        new_width = box.height * (1.0 - legend_side_ratio)
-        new_position = [box.x0, box.y0,
-                        new_width, box.height]
-
-        ax.set_position(new_position)
-
-        box = ax.get_position()
-        print("NEW Box w/h = (%f,%f)" % (box.width, box.height))
-
-    print("**legend_kwargs = %s" % legend_kwargs)
+     
+    #Make stacked bar chart
+    bottom = np.zeros(len(df.index), dtype='i')
+    for i, label in enumerate(df.columns):
+        counts = df[label]
+        color = biotype_colors[label]
+        _ = ax.bar(df.index, counts, label=label, color=color, bottom=bottom, linewidth=0)
+        bottom += counts
+    
+    #Format chart
+    ax.set_xlabel("Length (nt)")
+    ax.set_ylabel("Read counts")
+     
+    ax.set_ylim(ymin=0)
+    ax.set_xlim(xmin=(min(largs) - 0.7), xmax=(max(largs) + 0.7))
+    
+    #Shrink to fit legend
+    fig.tight_layout(rect=[0,0,0.7,1]) #left, bottom, right, top
+     
     ax.legend(**legend_kwargs)
-
+    mk_path_for_file(graph_image)
+     
     canvas = FigureCanvasAgg(fig)
     canvas.print_png(graph_image)
-
-
-def write_individual_bar_graphs(graph_image, largs, arrays, labels, colors, **kwargs):
-    """ same as write_stacked_bar_graph but writes out 1 plot per entry in array """
-
-    (name_base, extension) = os.path.splitext(graph_image)
-
-    title = kwargs.get("title", "")
-    y_label = kwargs.get("y_label", None)
-    for array, label, color in zip(arrays, labels, colors):
-        kwargs["title"] = "%s (%s)" % (title, label)
-        kwargs["color"] = color
-
-        bargraph = BarGraph(largs, array)
-        bargraph.y_label = y_label
-        individual_graph_image = "%s.%s%s" % (name_base, label, extension)  # extension still has dot
-        bargraph.save(individual_graph_image)
-
-
-class GraphBase(object):
-    __metaclass__ = abc.ABCMeta
-
-    def __init__(self, **kwargs):
-        """
-            legend: [('label', 'color'), ('label2', 'color2')]
-        """
-        self.title = kwargs.get("title")
-        self.x_label = kwargs.get("x_label")
-        self.y_label = kwargs.get("y_label")
-        self.legend = kwargs.get('legend')
-        self.plot_methods = [self.plot]
-
-    def decorations(self, ax):
-        if self.title:
-            ax.set_title(self.title)
-        if self.x_label:
-            ax.set_xlabel(self.x_label)
-        if self.y_label:
-            ax.set_ylabel(self.y_label)
-
-    @abc.abstractmethod
-    def plot(self, ax):
-        return
-
-    def post_plot(self, ax):
-        if self.legend:
-            patches = []
-            labels = []
-            for (name, color) in self.legend:
-                labels.append(name)
-                patches.append(Rectangle((0, 0), 1, 1, fc=color))
-
-            ax.legend(patches, labels, loc='upper left')
-
-    def figure(self, figure):
-        """ a hook method if you want to do something about the figure """
-        pass
-
-    def get_figure_and_axis(self, dpi):
-        figure = Figure(dpi=dpi)
-        figure.patch.set_facecolor('white')
-        ax = figure.add_subplot(1, 1, 1)
-        return figure, ax
-
-    def save(self, filename_or_obj, dpi=None, file_type='png'):
-        figure, ax = self.get_figure_and_axis(dpi)
-
-        self.decorations(ax)
-        for plot in self.plot_methods:
-            plot(ax)  # Implementation
-        self.post_plot(ax)
-        self.figure(figure)
-
-        canvas = FigureCanvasAgg(figure)
-        if file_type == 'png':
-            canvas.print_png(filename_or_obj)
-        elif file_type == 'pdf':
-            canvas.print_pdf(filename_or_obj)
-
-    def draw_origin(self, ax):
-        ax.axvline(c='black', lw=0.5)
-        ax.axhline(c='black', lw=0.5)
-
-    def colorbar_from_cmap_array(self, figure, cmap, array):
-        mappable = ScalarMappable(cmap=cmap)
-        mappable.set_array(array)
-        figure.colorbar(mappable)
-
-
-def write_csv_dict(csv_file, headers, rows, extrasaction=None, dialect=None):
-    """
-    default dialect = 'excel', other dialect option: 'excel-tab'
-    These are the same optional arguments as csv.DictWriter
-    headers=keys for dicts
-    rows=list of dicts
-    """
-
-    if extrasaction is None:
-        extrasaction = "raise"
-    if dialect is None:
-        dialect = 'excel'
-
-    f = file_or_file_name(csv_file, "w")
-
-    writer = csv.DictWriter(f, headers, extrasaction=extrasaction, dialect=dialect)
-    writer.writerow(dict(zip(headers, headers)))
-    writer.writerows(rows)
-
-
-class BarGraph(GraphBase):
-    def __init__(self, x, y, **kwargs):
-        super(BarGraph, self).__init__(**kwargs)
-        self.x = x
-        self.y = y
-        self.labels = kwargs.get("labels")
-        self.color = kwargs.get("color")
-
-    def plot(self, ax):
-        width = 0.5
-
-        ax.set_xlim(xmin=self.x[0], xmax=self.x[-1] + 1)
-        ax.bar(self.x, self.y, width=width, color=self.color)
-
-        if self.labels:
-            ax.set_xticks(self.x + width / 2)
-            ax.set_xticklabels(self.labels)
-
 
 if __name__ == '__main__':
     main()
